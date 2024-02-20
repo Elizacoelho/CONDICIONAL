2c04e32263abb7e4f4e2425e9d85becd99877547
Eliza <coelho.camargo@escola.pr.gov.br>
2024-02-20 11:59:10 -0300

    alterou

--- exercícios02.js
+++ exercícios02.js
@@ -1,5 +1,16 @@
-// 2- Escreva um programa que recebe três números e determina qual é o maior deles, usando if, else if e else:
+// 2- Escreva um programa que receba três números e determina qual é o maior deles, usando if, else if e else:
 
-let n = 4;
+let n = 14;
 let m = 8;
-let o = 13
+let o = 3;
+
+if (o>m || o>n) {
+    alert(o+" É o maior número");
+}
+else if (m>n || m>o) {
+    alert (m+ " É o maior");
+}
+
+else (n>o || n>m) {
+    alert (n+ " É o maior número entre eles");
+}
