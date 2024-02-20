6f8cd0b35ab3241ce7c1ce5b838a3d554fa3dade
Eliza <coelho.camargo@escola.pr.gov.br>
2024-02-20 11:29:50 -0300

    Exercícios 20/02

---
+++ exercícios01.js
@@ -0,0 +1,11 @@
+// 1- Crie um programa que verifica se um número é par ou ímpar. Use o operador % (resto da divisão) para fazer essa verificação:
+
+let numero = 3;
+if (numero % 2 === 0) {
+    alert ("O número é par");
+} else{
+    alert ("O número é ímpar");
+}
+
+
+
